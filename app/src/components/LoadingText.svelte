0f68fabf5289ea6928d2394beaa78f56ad0e6085
zees-dev <dev.786zshan@gmail.com>
2022-01-20 17:58:05 +1300

    laodingtext helper component

---
+++ app/src/components/LoadingText.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	// TODO: export width prop
+	// TODO: export height prop
+	// TODO: animate
+</script>
+
+<div class="p-4 text-gray-300">
+	<svg height="100%" width="100%">
+		<line x1="0" y1="4" x2="400" y2="4" />
+		<line x1="0" y1="34" x2="200" y2="34" />
+		<line x1="0" y1="64" x2="300" y2="64" />
+		<line x1="0" y1="94" x2="280" y2="94" />
+	</svg>
+</div>
+
+<style>
+	svg line {
+		stroke: currentColor;
+		stroke-width: 12;
+	}
+</style>

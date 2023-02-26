d92cd6f6ffcc92d89b1c28e3579f9804c7de3175
zees-dev <dev.786zshan@gmail.com>
2023-02-27 00:36:52 +1300

    updated types

--- client/src/components/endpoint/types.ts
+++ client/src/components/endpoint/types.ts
@@ -1,9 +1,14 @@
+
+export type ConnectionStatus = 'connecting' | 'connected' | undefined
+
 export interface Endpoint {
   date_added: string;
   enabled: boolean;
   id: string;
   name: string;
   rpc_url: string;
-  type: string;
+  type: 'http' | 'ws';
   user: string;
+  symbol: string;
+  block_explorer_url?: string,
 }

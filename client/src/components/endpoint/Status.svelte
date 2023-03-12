b3dc514f4e3c9fc37e91891abd83317cbc4a131a
zees-dev <dev.786zshan@gmail.com>
2023-03-12 13:58:27 +1300

    endpoint status page updated - to get endpoint based on request params

--- client/src/components/endpoint/Status.svelte
+++ client/src/components/endpoint/Status.svelte
@@ -1,8 +1,37 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+    import { dbStore } from "../../stores/db";
+    import Spinner from "../Spinner.svelte";
+  import { getEndpoint } from "./queries";
+    import type { Endpoint } from "./types";
+
   export let params: { endpointId: string };
 
+  let loading = true;
+  let endpoint: Endpoint | undefined = undefined;
+  let err: string | undefined | unknown = undefined;
+
+  onMount(() => getEndpointInComponent());
+
+  async function getEndpointInComponent() {
+    const { loading: l, result, error } = await getEndpoint($dbStore.db, params.endpointId);
+    loading = l;
+    endpoint = result;
+    err = error;
+  }
 </script>
 
 <div>
   Status {params.endpointId}!
+  {#if loading}
+    <Spinner size="lg" />
+  {:else if endpoint}
+    <div>
+      {endpoint.name}
+    </div>
+  {:else if err}
+    <div>
+      <p class="bg-error">Failed to fetch endpoint {params.endpointId}</p>
+    </div>
+  {/if}
 </div>
